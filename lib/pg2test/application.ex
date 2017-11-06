77376fc1212c918162685a108bbff8a60608a3c4
Mario Uher <uher.mario@gmail.com>
2017-11-06 09:53:49 +0100

    Make supervised app

---
+++ lib/pg2test/application.ex
@@ -0,0 +1,20 @@
+defmodule Pg2test.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Pg2test.Worker.start_link(arg)
+      # {Pg2test.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Pg2test.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
